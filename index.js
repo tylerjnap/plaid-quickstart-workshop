c6844d81a0ea6138f8d90697cc7f04dfb610c174
olympiawoj <owojcik7@gmail.com>
2021-10-21 11:13:46 -0400

    Create /token-exchange route

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const dotenv = require("dotenv").config();
+	const express = require('express')
+	const bodyParser = require('body-parser')
+	const dotenv = require("dotenv").config();
 
-const app = express()
+	const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
 
-const PORT = process.env.PORT || 3000;
+	const PORT = process.env.PORT || 3000;
 
-const path = require('path')
-const util = require('util')
+	const path = require('path')
+	const util = require('util')
 
-const plaid = require('plaid')
+	const plaid = require('plaid')
 
-// console.log(process.env.CLIENT_ID)
-// console.log(process.env.SECRET)
+	// console.log(process.env.CLIENT_ID)
+	// console.log(process.env.SECRET)
 
-const plaidClient = new plaid.Client({
-	clientID: process.env.CLIENT_ID,
-	secret: process.env.SECRET,
-	env: plaid.environments.development
-})
-
-app.get('/create-link-token', async(req, res)=>{
-	//rename link_token to linkToken
-	const {link_token: linkToken} = await plaidClient.createLink({
-		user: {
-			client_user_id: ''
-		},
-		client_name: 'App of Olympia',
-		products: ['auth', 'identity'],
-		country_codes: ['US'],
-		language: 'en'
+	const plaidClient = new plaid.Client({
+		clientID: process.env.CLIENT_ID,
+		secret: process.env.SECRET,
+		env: plaid.environments.development
 	})
-	res.json({linkToken})
-})
+
+	app.get('/create-link-token', async(req, res)=>{
+		//rename link_token to linkToken
+		const {link_token: linkToken} = await plaidClient.createLink({
+			user: {
+				client_user_id: process.env.CLIENT_ID
+			},
+			client_name: 'App of Olympia',
+			products: ['auth', 'identity'],
+			country_codes: ['US'],
+			language: 'en'
+		})
+		res.json({linkToken})
+	})
+
+	app.post('/token-exchange', async(req, res) =>{
+		//grab public token
+		const { publicToken} = req.body;
+		//exchange public token for access token
+		const {access_token:accessToken} = await plaidClient.exchangePublicToken(publicToken)
+		const authResponse = await plaidClient.getAuth(accessToken)
+		console.log('----------------')
+		console.log('Auth response: ')
+		console.log(util.inspect(authResponse, false, null, true))
+
+		const identityResponse = await plaidClient.getIdentity(accessToken)
+		console.log('----------------')
+		console.log('Identity response: ')
+		console.log(util.inspect(identityResponse, false, null, true))
+
+		const balanceResponse = await plaidClient.getBalance(accessToken)
+		console.log('----------------')
+		console.log('Balance response: ')
+		console.log(util.inspect(balanceResponse, false, null, true))
+
+		res.sendStatus(200)
+
+	})
 
 
-app.get('/', async(req, res)=> {
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
+	app.get('/', async(req, res)=> {
+		res.sendFile(path.join(__dirname, 'index.html'))
+	})
 
-app.listen(PORT, ()=>{ //callback function
-    console.log('listening on port: ', PORT)
-})
+	app.listen(PORT, ()=>{ //callback function
+		console.log('listening on port: ', PORT)
+	})
