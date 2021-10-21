595794b06d1e1f83db55aebe0c3280d36793413a
olympiawoj <owojcik7@gmail.com>
2021-10-21 10:15:32 -0400

    Initialize plaid client and create route /create-link-token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const dotenv = require("dotenv").config();
 
 const app = express()
 
@@ -10,6 +11,32 @@ const PORT = process.env.PORT || 3000;
 
 const path = require('path')
 const util = require('util')
+
+const plaid = require('plaid')
+
+// console.log(process.env.CLIENT_ID)
+// console.log(process.env.SECRET)
+
+const plaidClient = new plaid.Client({
+	clientID: process.env.CLIENT_ID,
+	secret: process.env.SECRET,
+	env: plaid.environments.development
+})
+
+app.get('/create-link-token', async(req, res)=>{
+	//rename link_token to linkToken
+	const {link_token: linkToken} = await plaidClient.createLink({
+		user: {
+			client_user_id: ''
+		},
+		client_name: 'App of Olympia',
+		products: ['auth', 'identity'],
+		country_codes: ['US'],
+		language: 'en'
+	})
+	res.json({linkToken})
+})
+
 
 app.get('/', async(req, res)=> {
     res.sendFile(path.join(__dirname, 'index.html'))
